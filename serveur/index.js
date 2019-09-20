f9692b962fa125a2b115c38fdbab745a635bbac7
AlaaBenk94 <a.benkarrad@gmail.com>
2019-09-20 13:14:45 +0200

    Assembler et servir le contenu
    * Configure default root
    * Add path constants
    * Add dev command script

--- serveur/index.js
+++ serveur/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const path = require('path');
+
+const DIST_DIR = path.join(__dirname, '../dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const mockResponse = {
@@ -8,9 +13,11 @@ const mockResponse = {
 app.get('/api', (req, res) => {
     res.send(mockResponse);
 });
+
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+    res.status(200).sendFile(HTML_FILE);
 });
+
 app.listen(port, function () {
     console.log('App listening on port: ' + port);
 });
