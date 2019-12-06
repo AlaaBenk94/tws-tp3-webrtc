13bebee8fd1312f401719c58b74ab8010fd9a022
Aissaoui <walid.ai@outlook.fr>
2019-12-06 11:24:34 +0100

    fixup! setup for tp3

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: ["babel-loader","eslint-loader"]
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
